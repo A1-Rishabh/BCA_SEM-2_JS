0339fa295b12925bb8aa656a1d90e72ce98bd648
A1-Rishabh <rishavbhardwaj2007@gmail.com>
2026-02-09 23:02:20 +0530

    making the color change along with the text in animation

--- practice/script.js
+++ practice/script.js
@@ -7,16 +7,16 @@ box.style.borderRadius = '50px';
 box.style.display = 'flex';
 box.style.justifyContent = 'center';
 box.style.alignItems = 'center';
-box.animate(
-    [
-        {transform: 'rotate(0deg)'},
-        {transform: 'rotate(360deg)'}
-    ],
-    {
-        duration: 3000,
-        iterations: Infinity
-    }
-);  
+// box.animate(
+//     [
+//         {transform: 'rotate(0deg)'},
+//         {transform: 'rotate(360deg)'}
+//     ],
+//     {
+//         duration: 3000,
+//         iterations: Infinity
+//     }
+// );  
 
 const boady = document.body;
 boady.style.margin='0';
@@ -34,6 +34,25 @@ button.style.margin = '20px auto';
 button.style.padding = '10px 20px';
 button.style.fontSize = '16px';
 button.style.cursor = 'pointer';
-button.addEventListener('click', () => {
-    alert('Button clicked!');
-});
+// button.addEventListener('click', () => {
+//     alert('Button clicked!');
+// });
+animateBox = () => {
+    box.animate(
+        [{transform: 'rotate(0deg)'},
+        {transform: 'rotate(360deg)'}
+    ],
+    {
+        duration: 3000,
+        iterations: 1,
+        easing: 'ease-in-out'
+    }
+    
+); 
+document.getElementById("bttn").textContent = "Animation started!";
+document.getElementById('box').style.backgroundColor = 'Red';
+setTimeout(() => {
+    document.getElementById("bttn").textContent = "Click Me";
+    document.getElementById('box').style.backgroundColor = 'lightblue';
+}, 3000);
+}
